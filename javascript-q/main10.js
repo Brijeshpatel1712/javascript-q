0010fb53171495e1c44a48c6dbc0964f3dd3e567
Brijeshpatel1712 <brijeshsangani088@gmail.com>
2025-04-06 21:53:47 +0530

    Add files via upload

---
+++ javascript-q/main10.js
@@ -0,0 +1,8 @@
+var apples = 8, 
+   oranges = 5;
+
+if (apples > oranges) {
+    console.log("The number of apples is greater than the number of oranges.");
+} else {
+    console.log("The number of apples is not greater than the number of oranges.");
+}
